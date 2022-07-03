27ecc5d0ed9e8d296a25d1b6a70812ba57978efe
RaphaelBento <rafhaesilvalbento@gmail.com>
2022-07-03 20:02:56 -0300

    primeiro commit
    calculadora feita em VueJs
    versão 2.x.x

---
+++ calculadora/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Olá mundo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#app{
+    background-color: blue;
+}
+</style>
